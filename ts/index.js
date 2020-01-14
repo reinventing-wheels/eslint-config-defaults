e90972ad9e063331974be3cdd5de46a754dd8509
- <5144598+-@users.noreply.github.com>
2020-01-14 17:32:52 +0300

    feat: support mixed codebases

--- ts/index.js
+++ ts/index.js
@@ -4,21 +4,25 @@ const js = require('../js')
 
 module.exports = {
   ...js,
-  extends: [
-    ...js.extends,
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    project: './tsconfig.json'
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    ...js.rules,
-    ...require('./overrides'),
-    ...require('./rules')
-  }
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        ...js.extends,
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        project: './tsconfig.json'
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        ...require('./overrides'),
+        ...require('./rules')
+      }
+    }
+  ]
 }
