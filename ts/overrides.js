6c334d3fcc702d851c0c86cebeb4ef78d773e743
- <5144598+-@users.noreply.github.com>
2020-03-26 06:50:19 +0300

    feat: switch to `no-unused-vars-experimental`

--- ts/overrides.js
+++ ts/overrides.js
@@ -8,13 +8,16 @@ const overrides = [
   'indent',
   'no-extra-parens',
   'no-extra-semi',
-  'no-unused-vars',
   'quotes',
   'semi',
   'space-before-function-paren'
 ]
 
-module.exports = Object.fromEntries(overrides.flatMap(rule => [
-  [rule, 'off'],
-  [`@typescript-eslint/${rule}`, js.rules[rule]]
-]))
+module.exports = Object.fromEntries([
+  ...overrides.flatMap(rule => [
+    [rule, 'off'],
+    [`@typescript-eslint/${rule}`, js.rules[rule]]
+  ]),
+  ['@typescript-eslint/no-unused-vars', 'off'],
+  ['@typescript-eslint/no-unused-vars-experimental', 'warn']
+])
