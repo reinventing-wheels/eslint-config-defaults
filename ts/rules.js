a16fe0bf0673a7240be75a9992a7379c5b05151e
- <5144598+-@users.noreply.github.com>
2020-08-23 22:03:31 +0300

    feat: disable some new rules

--- ts/rules.js
+++ ts/rules.js
@@ -4,6 +4,7 @@
 module.exports = {
   '@typescript-eslint/array-type': 'error',
   '@typescript-eslint/explicit-function-return-type': 'off',
+  '@typescript-eslint/explicit-module-boundary-types': 'off',
   '@typescript-eslint/member-delimiter-style': ['error', {
     multiline: {
       delimiter: 'none'
@@ -13,14 +14,18 @@ module.exports = {
     }
   }],
   '@typescript-eslint/no-explicit-any': 'off',
+  '@typescript-eslint/no-implied-eval': 'off',
   '@typescript-eslint/no-non-null-assertion': 'off',
-  '@typescript-eslint/no-unnecessary-condition': ['error', {
-    ignoreRhs: true
-  }],
   '@typescript-eslint/no-unnecessary-qualifier': 'error',
   '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+  '@typescript-eslint/no-unsafe-assignment': 'off',
+  '@typescript-eslint/no-unsafe-call': 'off',
+  '@typescript-eslint/no-unsafe-member-access': 'off',
+  '@typescript-eslint/no-unsafe-return': 'off',
   '@typescript-eslint/no-use-before-define': 'off',
   '@typescript-eslint/prefer-nullish-coalescing': 'error',
   '@typescript-eslint/prefer-optional-chain': 'error',
-  '@typescript-eslint/prefer-readonly': 'error'
+  '@typescript-eslint/prefer-readonly': 'error',
+  '@typescript-eslint/restrict-plus-operands': 'off',
+  '@typescript-eslint/restrict-template-expressions': 'off'
 }
